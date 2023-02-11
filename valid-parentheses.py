d701c0fe6f9ed4815d5578b0fe9cf4d0a41ec734
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-02-11 13:36:47 +0000

    Update valid-parentheses.py

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -1,5 +1,5 @@
 def isValid(s):
-        if len(s) % 2 != 0: #If there are an odd number of brackets then we know it is false automatically
+        if len(s) % 2 != 0: 
             return False
         
         if s[0] not in ["(", "[", "{"]:
@@ -18,40 +18,13 @@ def isValid(s):
 
         for c in s:
             if c in ["(", "[", "{"]:
-                open.append(c)
-            
+                open.append(c)            
             elif open:
                 if c == pairs[open[-1]]:
-                    open.pop()
-            
+                    open.pop()         
             else:
                 return False
-
-       
         return True if not open else False
-
-        
-        
-        start = s[0]
-        middle = []
-        close = pairs[start]
-        s = s[1:]
-
-        while True:
-            for i, char in enumerate(s):
-                if char == close:
-                    
-                    count = [middle.count(char) for char in set(middle)]
-                
-                    for item in count:
-                        if item % 2 == 0:
-                            return False
-                    break
-                else:
-                    middle.append(char)
-   
-       
-       
 
 
 if __name__ == "__main__":
