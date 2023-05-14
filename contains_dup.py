20cf4167bc1df36b5ca1180939c63414520068e4
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-05-14 17:16:40 +0100

    Update contains_dup.py

--- contains_dup.py
+++ contains_dup.py
@@ -1,1 +1,11 @@
-return len(set(nums)) != len(nums)        
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict_ = {}
+
+        for elm in nums:
+            status = dict_.get(elm, False)
+            if not status:
+                dict_[elm] = 1
+            else:
+                return True
+        return False
