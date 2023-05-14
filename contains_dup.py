e52f76fdc99cc39dfc2049f0745a35730427e6c2
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-05-14 17:14:51 +0100

    Add files via upload

--- contains_dup.py
+++ contains_dup.py
@@ -1,10 +1,1 @@
-nums = [1,2,3,1]
-dict_ = {}
-
-for elm in nums:
-    status = dict_.get(elm, False)
-    if not status:
-        dict_[elm] = 1
-    else:
-        print(False)
-print(True)
+return len(set(nums)) != len(nums)        
