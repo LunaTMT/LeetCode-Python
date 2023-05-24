7cee6d80aab1b3ff86f3dcb26ea255cee91af817
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-05-24 22:29:56 +0100

    Add files via upload

---
+++ Number_of_Good_Pairs.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def numIdenticalPairs(self, nums: list[int]) -> int:
+
+        res = 0
+        for n in Counter(nums).values():
+            if n > 1:
+                n -= 1
+                res += n*(n+1) / 2
+        return int(res)
