4f3f846bce4d1f0b37dd999ca3add13f5687a899
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-06-04 17:39:32 +0100

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,4 +1,5 @@
 def isPalindrome(s: str) -> bool:
+    #Two pointer
     i, j = 0, len(s) - 1
     while i < j:
         a, b = s[i].lower(), s[j].lower()
@@ -10,5 +11,10 @@ def isPalindrome(s: str) -> bool:
         i, j = i + (not a.isalnum()), j - (not b.isalnum())
     return True
 
+    #Simple 
+    s = s.lower()
+    s = re.sub(r'[\W_]', '', s)
+    return s == s[::-1]
+        
 if __name__ == "__main__": 
     isPalindrome("A man, a plan, a canal: Panama")
