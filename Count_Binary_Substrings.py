6ff906fc22e48f14fa049d3d1843bda8b45eb0d9
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-06-27 21:39:05 +0100

    Add files via upload

---
+++ Count_Binary_Substrings.py
@@ -0,0 +1,37 @@
+def countBinarySubstrings(s: str) -> int:
+    #s = '_' + s + '_'
+
+
+    l = r = 0
+    
+    current_count = 0
+    opposite_count = 0
+    res = 0
+    while l < len(s)-1:
+        current = s[l]
+        
+        try:
+            while s[r] == current:
+                current_count += 1
+                r += 1
+        except:
+            pass
+        
+        l = r
+
+        try:
+            while s[r] != current:
+                opposite_count += 1
+                r += 1
+        except:
+            pass
+
+        
+        r = l
+
+        res += min(current_count, opposite_count)
+        current_count = opposite_count = 0
+
+
+if __name__ == "__main__":
+    countBinarySubstrings("00110011")
