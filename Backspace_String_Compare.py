a82ac86733006c249dce055fb4573a51e0469f81
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-06-30 13:22:00 +0100

    Add files via upload

---
+++ Backspace_String_Compare.py
@@ -0,0 +1,26 @@
+
+
+def backspaceCompare(s: str, t: str) -> bool:
+
+    s = [*s]
+    t = [*t]
+
+    def remove_hash_tag(s):
+        remove = 0
+        r = len(s)-1
+        while remove >= 0:
+            if s[r] == "#":
+                remove += 1
+                s.pop(r)
+            
+            elif remove > 0:
+                remove -= 1
+                s.pop(r)
+
+            r -= 1  
+        return s
+        
+    return remove_hash_tag(s) == remove_hash_tag(t)
+
+backspaceCompare("a#c", "b")
+            
