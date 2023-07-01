fc773e2a11771f869ad4de1419a664eaace16db5
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-01 15:45:41 +0100

    Update Merge_Sorted_Array.py

--- Merge_Sorted_Array.py
+++ Merge_Sorted_Array.py
@@ -1,26 +1,17 @@
-nums1 = [4, 5, 6, 0, 0, 0]
-nums2 = [1, 2, 3]
-m = 3
-n = 3
-
-R1 = m-1
-R2 = n-1
-i = (m+n)-1
-
-while R1 >= 0 and R2 >= 0:
-    n1_val = nums1[R1]
-    n2_val = nums2[R2]
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     
+    R1 = m-1
+    R2 = n-1
+    i = (m+n)-1
 
-    if n1_val > n2_val:
-        nums1[i] = n1_val
-        R1 -= 1
-    else:
-        nums1[i] = n2_val
-        R2 -= 1
-    i -= 1
+    while R1 >= 0 and R2 >= 0:
+        if nums1[R1] > nums2[R2]:
+            nums1[i] = nums1[R1]
+            R1 -= 1
+        else:
+            nums1[i] = nums2[R2]
+            R2 -= 1
+        i -= 1
 
-if R2 >= 0:
-    nums1[:R2] = nums2[:R2]
-
-print()
+    if R2 >= 0:
+        nums1[:R2+1] = nums2[:R2+1]
