dfca8c66d3faf4e6019fbc20abbe85d4b82018a2
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-01 16:53:36 +0100

    Add files via upload

---
+++ Valid_Palindrome_II.py
@@ -0,0 +1,16 @@
+def validPalindrome(s: str) -> bool:        
+    def validate(l, r, s, deleted=False):
+        while l < r:
+            if s[l] != s[r]:
+                if deleted:
+                    return False
+                else:
+                    return validate(l+1, r, s, True) or validate(l, r+1, s, True)
+            else:
+                l, r = l+1, r-1
+        return True
+    
+    return validate(0, len(s)-1, s)
+
+check = validPalindrome("abc")
+print()
