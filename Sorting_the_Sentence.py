6236c8131d08afa59e77ec89984b09f95f4a9c34
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-17 14:14:37 +0100

    Add files via upload

---
+++ Sorting_the_Sentence.py
@@ -0,0 +1,10 @@
+def sortSentence(self, s: str) -> str:
+    s = s.split(" ")
+    s.sort(key=lambda x: x[-1])
+    
+    ans = ""
+    for word in s:
+        ans += word[:-1] + " "
+    
+    return ans[:-1]
+        
