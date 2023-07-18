1db70b3f4d2e6574f08bef673189535b75d3850e
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-18 13:31:52 +0100

    Update Replace_All_Digits_with_Characters.py

--- Replace_All_Digits_with_Characters.py
+++ Replace_All_Digits_with_Characters.py
@@ -1,3 +1,4 @@
+#https://leetcode.com/problems/replace-all-digits-with-characters/solutions/3782923/python-simplest-solution-with-detailed-explanation-beats-97-94/
 def replaceDigits(self, s: str) -> str:
     s = [*s]
     for i in range(1, len(s), 2):
