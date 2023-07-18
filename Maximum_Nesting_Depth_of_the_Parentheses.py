cfc65d322485baf41827b80587fec6436fa48610
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-18 11:43:38 +0100

    Add files via upload

---
+++ Maximum_Nesting_Depth_of_the_Parentheses.py
@@ -0,0 +1,9 @@
+def maxDepth(self, s: str) -> int:
+    res = cur = 0
+    for c in s:
+        if c  == "(":
+            cur += 1
+            res = max(res, cur)
+        elif c == ")":
+            cur -= 1
+    return res
