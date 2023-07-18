5d5601561df80e2e9fea503b791f9485fbf3791f
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-18 13:14:43 +0100

    Update Sort_the_People.py

--- Sort_the_People.py
+++ Sort_the_People.py
@@ -1,3 +1,4 @@
+#https://leetcode.com/problems/sort-the-people/solutions/3782856/by-far-the-simplest-python-solution-beats-94-02-3-lines/
 def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
     zipped = list(zip(names, heights))
     zipped.sort(key=lambda x: x[1], reverse=True)
