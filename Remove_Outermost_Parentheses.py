e37c263600e1abc641501fd8dc73a8679fdc8967
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-18 12:19:07 +0100

    Update Remove_Outermost_Parentheses.py

--- Remove_Outermost_Parentheses.py
+++ Remove_Outermost_Parentheses.py
@@ -1,20 +1,29 @@
 
 def removeOuterParentheses( s: str) -> str:
     open_br = 0
-
     res = ""
     sub_str = ""
     for c in s:
-        if c == "(":
-            open_br += 1
-        elif c == ")":
-            open_br -= 1
-        
+        open_br += 1 if c == "(": else - 1
         sub_str += c
     
         if open_br == 0:
             res += sub_str[1:][:-1]
             sub_str = ""
     return res
+
+
+    """
+    OR
+    open_br = 0
+    res = []
+    
+    for c in s:
+        if c == "(" and open_br > 0: res.append(c)
+        if c == ")" and open_br > 1: res.append(c)
+        open_br += 1 if c == "(" else - 1
+
+    return "".join(res)
+    """
 
 removeOuterParentheses("(()()) (()) ")
