cd6f86b4c1113671f8d932571ee084889612f722
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-23 14:22:25 +0100

    Update Determine_if_String_Halves_Are_Alike.py

--- Determine_if_String_Halves_Are_Alike.py
+++ Determine_if_String_Halves_Are_Alike.py
@@ -6,14 +6,12 @@ def halvesAreAlike(s: str) -> bool:
     s2 = s[mid:]
 
     s1_count = s2_count = 0
-    print(s1, s2)
     for v1, v2 in zip(s1, s2):
         if v1 in vowels: 
             s1_count += 1
-        if v1 in vowels:
+        if v2 in vowels:
             s2_count += 1
-    
-    print(s1_count, s2_count)
+
     return s1_count == s2_count
 
 halvesAreAlike("textbook")
