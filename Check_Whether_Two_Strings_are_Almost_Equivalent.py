49ade87da09e0e367cdf316bd70a592a9c3cf122
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-05 13:34:37 +0100

    Add files via upload

--- Check_Whether_Two_Strings_are_Almost_Equivalent.py
+++ Check_Whether_Two_Strings_are_Almost_Equivalent.py
@@ -1,14 +1,13 @@
 from collections import Counter
 
 def checkAlmostEquivalent(w1: str, w2: str) -> bool:
-    w1_count = Counter(w1)
-    w2_count = Counter(w2)
+        w1 = Counter(w1)
+        w2 = Counter(w2)
+        d1 = (w1 - w2)
+        d2 = (w2 - w1)
+        diff = d1 + d2
+        return all(v <= 3 for v in diff.values())
 
-    letters = set(w1).union(set(w2))
-    for c in letters:
-        if abs(w1_count[c] - w2_count[c]) > 3:
-            return False
-    return True
 
 w1 = "abcdeef"
 w2 = "abaaacc"
