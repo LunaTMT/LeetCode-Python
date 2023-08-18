aca6d9ad0b973fcb3308164e7a0dca78d2ddd1e1
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-18 12:40:55 +0100

    Add files via upload

---
+++ Verifying_an_Alien_Dictionary.py
@@ -0,0 +1,6 @@
+def isAlienSorted(words, order):
+    m = {c: i for i, c in enumerate(order)}
+    words = [[m[c] for c in w] for w in words]
+    return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
+
+isAlienSorted(words = ["word","world","row", "rows", "rxxxx"], order = "worldabcefghijkmnpqstuvxyz")
