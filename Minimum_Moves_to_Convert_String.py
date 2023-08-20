b213e9efbaaea53edd1967fdc5d906e7e0cc29e7
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-20 21:36:58 +0100

    Add files via upload

---
+++ Minimum_Moves_to_Convert_String.py
@@ -0,0 +1,10 @@
+def minimumMoves(s: str) -> int:
+    i = 0
+    res = 0
+    while i < len(s):
+        if s[i] == "X":
+            res += 1
+            i += 3
+        else:
+            i += 1
+    return res
