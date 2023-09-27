e4481e6906e34088605185b565a8751af81e4fe2
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-09-27 13:40:35 +0100

    Update Number_of_Different_Integers_in_a_String.py

--- Number_of_Different_Integers_in_a_String.py
+++ Number_of_Different_Integers_in_a_String.py
@@ -1,3 +1,2 @@
 def numDifferentIntegers(self, s: str) -> int:
-    return len(set(int(i) for i in re.findall(r"\d+", s) if i.isnumeric()))
-
+    return len(set(int(i) for i in re.findall(r"\d+", s)))
