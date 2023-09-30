803954da1a2af25591d9dd9157ee24842db87604
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-09-30 13:56:58 +0100

    Update Create_Target_Array_in_the_Given_Order.py

--- Create_Target_Array_in_the_Given_Order.py
+++ Create_Target_Array_in_the_Given_Order.py
@@ -11,4 +11,12 @@ def createTargetArray(nums: list[int], index: list[int]) -> list[int]:
 
 createTargetArray(nums = [4,2,4,3,2], index = [0,0,1,3,1])
 
+"""
+OR
+        target=[]
+        for i in range(len(nums)):
+            target.insert(index[i],nums[i])
+        return target
+"""
+
 #[2,2,4,4,3]
