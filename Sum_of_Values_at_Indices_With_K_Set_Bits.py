2da24b2d02a556f3d17fde3de400cc5694e092ae
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-09-30 12:28:16 +0100

    Update Sum_of_Values_at_Indices_With_K_Set_Bits.py

--- Sum_of_Values_at_Indices_With_K_Set_Bits.py
+++ Sum_of_Values_at_Indices_With_K_Set_Bits.py
@@ -1,3 +1,13 @@
 def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
     return sum([nums[i] for i in range(len(nums)) if bin(i).count('1') == k])
+
+"""
+OR
+
+     res = 0
+        for i, num in enumerate(nums):
+            if i.bit_count() == k:
+                res += num
+        return res
+"""
     
