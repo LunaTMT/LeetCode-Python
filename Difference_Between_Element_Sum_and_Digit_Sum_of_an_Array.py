3a3341087a712a310f99cab222fda3bd6e629544
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-10-02 20:02:58 +0100

    Add files via upload

---
+++ Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.py
@@ -0,0 +1,11 @@
+def differenceOfSum(self, nums: List[int]) -> int:
+    element = 0
+    digit = 0
+    for num in nums:
+        element += num
+        
+        while num > 0:
+            digit += (num % 10)
+            num //= 10
+            
+    return element - digit
