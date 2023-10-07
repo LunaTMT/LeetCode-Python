5a3bb02ba3613d275bfcdd47115cd9e395997fd3
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-10-07 12:51:07 +0100

    Add files via upload

--- Array_Partition.py
+++ Array_Partition.py
@@ -1,3 +1,8 @@
-def arrayPairSum(self, nums: list[int]) -> int:
+def arrayPairSum(nums: list[int]) -> int:
     nums.sort()
     return sum(nums[i] for i in range(0, len(nums), 2))
+
+    
+    return sum(sorted(nums)[::2])
+
+arrayPairSum([1,4,3,2])
