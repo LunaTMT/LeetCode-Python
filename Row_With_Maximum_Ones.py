2ab6fa95764c21070d5ebdb6962888a1fccd29e8
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-10-10 12:11:35 +0100

    Add files via upload

--- Row_With_Maximum_Ones.py
+++ Row_With_Maximum_Ones.py
@@ -7,3 +7,13 @@ def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
             max_ = ones
             row = i
     return (row, max_)
+
+    """
+    OR
+    """
+
+    ones = []
+    for i, r in enumerate(mat):
+        ones.append([i, r.count(1)])
+    ones.sort(key=lambda x:[-x[1],x[0]])
+    return ones[0]
