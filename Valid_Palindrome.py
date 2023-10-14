2565899675f3849b2f5a52ba4d16ebb6b549642d
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-10-14 12:01:28 +0100

    Add files via upload

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -1,3 +1,20 @@
 def isPalindrome(s: str) -> bool:
     s = "".join(c.lower() for c in s if c.isalnum())
     return s == s[::-1]
+
+    """
+    O(1) space (two-pointer)
+    """
+
+    i, j = 0, len(s)-1
+
+    while i < j:
+        a, b = s[i].lower(), s[j].lower()
+        if a.isalnum() and b.isalnum():
+            if a != b: return False
+            else:
+                i += 1
+                j -= 1
+        i += not a.isalnum()
+        j -= not b.isalnum() 
+    return True
