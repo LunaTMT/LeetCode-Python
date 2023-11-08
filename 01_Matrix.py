d35e987c4f942b96a9b4905e110f30afc6ec08bc
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-11-08 13:08:55 +0000

    Add files via upload

--- 01_Matrix.py
+++ 01_Matrix.py
@@ -1,34 +1,26 @@
-def updateMatrix( mat: list[list[int]]) -> list[list[int]]:
-    rows = len(mat)
-    columns = len(mat[0])
-    res = []
+from collections import deque
 
-    def rec(sr, sc, r, c): 
-        if not (0 <= r < rows) or not (0 <= c < columns): return
-        
-        if mat[r][c] == 0: 
-            distance = min(abs(sr - r), abs(sc - c))
-            if distance < mat[sr][sc]:
-                mat[sr][sc] = distance
-                return
+"""So hard to understand    """
 
-        rec(sr, sc, r-1, c)
-        rec(sr, sc, r+1, c)
-        rec(sr, sc, r, c-1)
-        rec(sr, sc, r, c+1)
+def updateMatrix(mat: list[list[int]]) -> list[list[int]]:
+    rows, columns = len(mat), len(mat[0])
+    DIR = [(0,1),(1,0),(0,-1),(-1,0)] # RDLU
 
-        #distance = min(abs(r - closest[0]), abs(c - closest[1]))
-
-    
-
-    for sr in range(rows):
-        row = []
-        for sc in range(columns):
-            if mat[sr][sc] == 0:
-                row.append(0)
+    q = deque([])
+    for r in range(rows):
+        for c in range(columns):
+            if mat[r][c] == 0:
+                q.append((r, c))
             else:
-                rec(sr, sc, sr, sc)
-        res.append(row)
-    return res
+                mat[r][c] = -1  # Marked as not processed yet!
 
-updateMatrix(mat = [[0,0,0],[0,1,0],[0,0,0]])
+    while q:
+        r, c = q.popleft()
+        for a,b in DIR:
+            nr,nc = a+r, b+c
+            if nr < 0 or nr == rows or nc < 0 or nc == columns or mat[nr][nc] != -1: continue
+            mat[nr][nc] = mat[r][c] + 1
+            q.append((nr, nc))
+    return mat
+
+updateMatrix(mat = [[0,0,0],[0,1,0],[1,1,1]])
